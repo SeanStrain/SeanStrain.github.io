5a472de25092fe6df52d072d524b4dc0c9fa423b
Sean Strain <s1832137@ed.ac.uk>
2023-04-22 00:21:39 +0100

    menu leaves if click canvas

--- Attractors/userInput.mjs
+++ Attractors/userInput.mjs
@@ -4,8 +4,14 @@ let deg2 = 0;
 
 document.getElementById("menu-button").addEventListener("click", () => 
 {
+    var listener = false
     if (enabled)
     {
+        if (listener)
+        {
+            document.getElementById('body').removeEventListener("click", listener);
+            listener = false;
+        }
         const menu1 = document.getElementById("menu-1");
         const height = parseInt(window.getComputedStyle(menu1).getPropertyValue('height'.split("px")[0]));
         deg1 = 0;
@@ -34,6 +40,39 @@ document.getElementById("menu-button").addEventListener("click", () =>
     }
     else
     {
+        listener = document.getElementById('canvas').addEventListener("click", () =>
+        {
+            if (enabled)
+            {
+                const menu1 = document.getElementById("menu-1");
+                const height = parseInt(window.getComputedStyle(menu1).getPropertyValue('height'.split("px")[0]));
+                deg1 = 0;
+                deg2 = 0;
+                enabled = false;
+                gsap.to("#menu-2", 
+                {
+                    duration: 0.3, 
+                    opacity: 1,
+                    ease: "power2.out"
+                });
+            
+                gsap.to("#menu-1",
+                {
+                    duration: 0.5,
+                    transform: `rotate3d(0, 0, 1, ${deg1}deg) translateY(${10 + height}px)`,
+                    ease: "power2.out"
+                });
+                gsap.to("#menu-3",
+                {
+                    duration: 0.5,
+                    transform: `rotate3d(0, 0, 1, ${deg2}deg) translateY(-${10 + height}px)`,
+                    ease: "power2.out"
+                });
+                
+            }
+            document.getElementById("menu").classList.remove("open");
+        });
+
         enabled = true;
         deg1 = 45;
         deg2 = -45;
